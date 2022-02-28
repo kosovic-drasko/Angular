3dd7f52417ecfe19987fcbc5711f0144523ac07b
drasko1959 <drasko.kosovic@gmial.com>
2022-02-28 13:32:53 +0100

    init

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 import { ProbaService } from './proba.service';
 
 @Component({
@@ -7,7 +8,7 @@ import { ProbaService } from './proba.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-
+  chart:any = [];
   constructor(private servisProba:ProbaService) {
   }
   ngOnInit(): void {
@@ -20,21 +21,37 @@ this.podaci();
 
      let temp_max = res
      console.log(temp_max);
-     let temp_min = res.map((res: { title: any; }) => res.title)
+     let naslov = res.map((res: { title: any; }) => res.title)
+     let id = res.map((res: { id: any; }) => res.id)
      // let alldates = res['list'].map(res => res.dt)
-console.log(temp_min);
-   })
-  };
+console.log(naslov);
+     this.chart = new Chart('canvas', {
+       type: 'line',
+       data: {
+         labels: naslov,
+         datasets: [
+           {
+             data: id,
+             borderColor: '#3cba9f',
+             fill: true
+           },
+         ]
+       }
 
-  public barChartOptions = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType = 'bar';
-  public barChartLegend = true;
-  public barChartData = [
-    {data: [105, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  ];
+   })
+  })
+  }
+  //
+  //
+  // public barChartOptions = {
+  //   scaleShowVerticalLines: false,
+  //   responsive: true
+  // };
+  // public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  // public barChartType = 'bar';
+  // public barChartLegend = true;
+  // public barChartData = [
+  //   {data: [105, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+  //   {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  // ];
 }
