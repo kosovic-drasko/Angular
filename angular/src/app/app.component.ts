a5b89bbd944ac13300e9d9d689a306334fe6e54f
drasko1959 <drasko.kosovic@gmial.com>
2022-03-01 07:10:12 +0100

    init

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -37,21 +37,23 @@ console.log(naslov);
            },
          ]
        }
+       ,
+       options: {
+
+         scales: {
+           xAxes: {
+             display: true
+           },
+           yAxes: {
+             display: true
+           }
+         }
+       }
+     })
 
    })
-  })
   }
-  //
-  //
-  // public barChartOptions = {
-  //   scaleShowVerticalLines: false,
-  //   responsive: true
-  // };
-  // public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  // public barChartType = 'bar';
-  // public barChartLegend = true;
-  // public barChartData = [
-  //   {data: [105, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-  //   {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  // ];
 }
+
+
+
